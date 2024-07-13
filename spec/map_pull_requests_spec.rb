1518b177eb4bb626cc090f9af272f7a4749a54d1
Zsolt Benke <zsolt@decoding.io>
2024-07-13 14:19:47 +0200

    Remove failing spec

--- spec/map_pull_requests_spec.rb
+++ spec/map_pull_requests_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe MapPullRequests do
   it "has a version number" do
     expect(MapPullRequests::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
